385441c7a19c4c97993660a33d22537704c3cf45
elsahuaro <elsahuaro2020@gmail.com>
2021-01-29 15:07:39 -0700

    Agregar capacitación

--- src/js/calendario-capacitacion/index.js
+++ src/js/calendario-capacitacion/index.js
@@ -75,6 +75,7 @@ cal_data = {
     },
     1: {
       4: ["Capacitación de RC del Distrito 3. 19:00 hrs."],
+      5: ["Capacitación de RC del Distrito 3 presencial en Colonia Cuatro Olivos de Hermosillo. 18:00 hrs."],
       7: ["Capacitación de RC del Distrito 3 presencial en Poblado Miguel Alemán. 16:00 hrs."]
     }
   }
