aa2d6756e8b1bc0638a3fec74aa1edd020a02c51
elsahuaro <elsahuaro2020@gmail.com>
2021-02-12 11:57:19 -0700

    Revertir cambio de capacitación D3 presencial en poblado miguel alemán

--- src/js/calendario-capacitacion/index.js
+++ src/js/calendario-capacitacion/index.js
@@ -100,8 +100,8 @@ cal_data = {
            "Capacitación de RC del Distrito 1. 19:30 hrs."],
       14: ["Capacitación de RG del Distrito 7 presencial en Comité de Morena de Navojoa. 9:00 hrs.",
            "Capacitación de RG del Distrito 7 presencial en Huatabampo. 15:00 hrs.",
-           "Capacitación de RC del Distrito 3. 17:00 hrs."],
-      21: ["Capacitación de RC del Distrito 3 presencial en Poblado Miguel Alemán. 16:00 hrs."]
+           "Capacitación de RC del Distrito 3 presencial en Poblado Miguel Alemán. 16:00 hrs.",
+           "Capacitación de RC del Distrito 3. 17:00 hrs."]
     }
   }
 };
