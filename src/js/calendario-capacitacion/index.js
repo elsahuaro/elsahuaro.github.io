03520f4c8fd6e4030281343df4be15d50014b8e4
elsahuaro <elsahuaro2020@gmail.com>
2021-02-23 12:43:28 -0700

    Modificar capacitaciones D6 para ser de RC y RG

--- src/js/calendario-capacitacion/index.js
+++ src/js/calendario-capacitacion/index.js
@@ -154,8 +154,8 @@ cal_data = {
 	   "Capacitación de RC del Distrito 7 presencial en Oficina de Morena en Navojoa. 16:00 hrs.",
 	   "Capacitación de RC del Distrito 7 presencial en Oficina de Morena en Etchojoa. 17:00 hrs."],
       23: ["Capacitación de RC del Distrito 7 presencial en Oficina de Morena en Etchojoa. 16:00 hrs.",
-	   "Capacitación de RC del Distrito 6 presencial en Col. Casa Blanca en Cd. Obregón. 16:00 hrs."],
-      24: ["Capacitación de RC del Distrito 6 presencial en Calle Juárez en Esperanza. 17:00 hrs."],
+	   "Capacitación de RC/RG del Distrito 6 presencial en Col. Casa Blanca en Cd. Obregón. 16:00 hrs."],
+      24: ["Capacitación de RC/RG del Distrito 6 presencial en Calle Juárez en Esperanza. 17:00 hrs."],
       25: ["Capacitación de RG del Distrito 2 presencial en Nogales. 11:00 hrs."],
       26: ["Capacitación de RG del Distrito 2 presencial en Nogales. 11:00 hrs."],
       28: ["Capacitación de RC del Distrito 3 presencial en Poblado Miguel Alemán. 17:00 hrs."]
