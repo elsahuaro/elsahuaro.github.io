f3635824329cc4eeb9cb27d6575aa9c2d357a167
elsahuaro <elsahuaro2020@gmail.com>
2021-02-23 17:07:28 -0700

    Actualizar información de distribución distrital

--- src/js/distribucion-distrital/index.js
+++ src/js/distribucion-distrital/index.js
@@ -1,18 +1,18 @@
 var info = [
-  { "lnominal": 288072, "secciones": 178, "urbanas": 136, "nourbanas": 42,
-    "municipios": 11, "dels": 3, "casillas": 496, "curbanas": 391, "cnourbanas": 105 },
-  { "lnominal": 333881, "secciones": 226, "urbanas": 186, "nourbanas": 40,
-    "municipios": 11, "dels": 4, "casillas": 563, "curbanas": 487, "cnourbanas": 76 },
-  { "lnominal": 304073, "secciones": 215, "urbanas": 201, "nourbanas": 14,
-    "municipios": 1, "dels": 5, "casillas": 503, "curbanas": 473, "cnourbanas": 30 },
-  { "lnominal": 291131, "secciones": 286, "urbanas": 135, "nourbanas": 151,
-    "municipios": 41, "dels": 4, "casillas": 547, "curbanas": 296, "cnourbanas": 251 },
-  { "lnominal": 295985, "secciones": 202, "urbanas": 183, "nourbanas": 19,
-    "municipios": 1, "dels": 5, "casillas": 501, "curbanas": 454, "cnourbanas": 47 },
-  { "lnominal": 320458, "secciones": 219, "urbanas": 189, "nourbanas": 30,
-    "municipios": 1, "dels": 3, "casillas": 552, "curbanas": 449, "cnourbanas": 103 },
-  { "lnominal": 268126, "secciones": 207, "urbanas": 110, "nourbanas": 97,
-    "municipios": 7, "dels": 3, "casillas": 474, "curbanas": 240, "cnourbanas": 234 }
+  { "lnominal": 295892, "secciones": 176, "urbanas": 136, "nourbanas": 40,
+    "municipios": 11, "dels": 3, "casillas": 509, "curbanas": 401, "cnourbanas": 108 },
+  { "lnominal": 347470, "secciones": 221, "urbanas": 186, "nourbanas": 35,
+    "municipios": 11, "dels": 4, "casillas": 595, "curbanas": 513, "cnourbanas": 82 },
+  { "lnominal": 323490, "secciones": 214, "urbanas": 201, "nourbanas": 13,
+    "municipios": 1, "dels": 5, "casillas": 536, "curbanas": 498, "cnourbanas": 38 },
+  { "lnominal": 289546, "secciones": 276, "urbanas": 133, "nourbanas": 143,
+    "municipios": 41, "dels": 4, "casillas": 551, "curbanas": 292, "cnourbanas": 259 },
+  { "lnominal": 307942, "secciones": 199, "urbanas": 182, "nourbanas": 17,
+    "municipios": 1, "dels": 5, "casillas": 522, "curbanas": 473, "cnourbanas": 49 },
+  { "lnominal": 320299, "secciones": 231, "urbanas": 199, "nourbanas": 32,
+    "municipios": 1, "dels": 3, "casillas": 559, "curbanas": 471, "cnourbanas": 88 },
+  { "lnominal": 271610, "secciones": 205, "urbanas": 90, "nourbanas": 115,
+    "municipios": 7, "dels": 3, "casillas": 480, "curbanas": 243, "cnourbanas": 237 }
 ];
 
 function infoSeleccion(i) {
